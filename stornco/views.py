5da37992af01cb667dc2f9c21185a956aca5e367
Storn White <storn@partender.com>
2017-01-21 17:36:12 -0800

    Hello World!

---
+++ stornco/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+
+
+def home(request):
+    """
+    Homepage for storn.co.
+
+    :param request: HttpRequest object
+    :return: HttpResonse object
+    """
+    return HttpResponse("Welcome to StornCo.  Get Some!")
