aa6bd3d311b24bca02540866176d6c6c11492914
Storn White <storn@partender.com>
2017-01-23 22:08:26 -0800

    Add local db settings

--- stornco/settings/local.py
+++ stornco/settings/local.py
@@ -1,2 +1,19 @@
 # Local development environment settings
+import os
+
 from .base import *
+
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ('DB_ENGINE'),
+        'HOST': os.environ('DB_HOST'),
+        'PORT': os.environ('DB_PORT'),
+        'NAME': os.environ('DB_NAME'),
+        'USER': os.environ('DB_USER'),
+        'PASSWORD': os.environ('DB_PASSWORD'),
+    }
+}
