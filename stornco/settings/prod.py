273798eea9e15e4d93c61c4c6c2acf57535b5d0a
Storn White <stornwhite@Storns-MacBook-Pro.local>
2017-01-28 08:53:45 -0800

    Security improvements to production settings

--- stornco/settings/prod.py
+++ stornco/settings/prod.py
@@ -1,9 +1,8 @@
 # Production environment settings
 from .base import *
 
-# Todo: Remove elasticbeanstalk host after configuring DNS
-# Todo: Explicitly list allowed hosts after configuring DNS
+DEBUG = False
+
 ALLOWED_HOSTS = [
-    '.us-west-1.elasticbeanstalk.com',
     '.storn.co'
 ]
