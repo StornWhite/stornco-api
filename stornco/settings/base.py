b152c54ffee060d65ef949581d7021e4688fb900
Storn White <stornwhite@Storns-MacBook-Pro.local>
2017-01-28 22:22:51 -0800

    Add S3 settings for prod

--- stornco/settings/base.py
+++ stornco/settings/base.py
@@ -21,14 +21,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # project apps
     'contact.apps.ContactConfig',
     'authorize.apps.AuthorizeConfig',
+
+    # django-storages (amazone S3 storage engine)
+    'storages',
 ]
 
 MIDDLEWARE = [
--- stornco/settings/prod.py
+++ stornco/settings/prod.py
@@ -1,8 +1,26 @@
 # Production environment settings
+import os
 from .base import *
+
+# For security:
 
 DEBUG = False
 
 ALLOWED_HOSTS = [
     '.storn.co'
 ]
+
+
+# Static file stuff:
+
+# Production static file directories are relative to:
+STATIC_ROOT = "stornco-prod-static.s3-website-us-west-1.amazonaws.com/"
+STATIC_URL = "stornco-prod-static.s3-website-us-west-1.amazonaws.com/"
+
+# Collects static files to s3.
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3Boto3Storage'
+
+# S3 bucket identity:
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
