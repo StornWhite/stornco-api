b85a9afbfdc8bb6b170a21e521003f6e42625a6d
Storn White <stornwhite@Storns-MacBook-Pro.local>
2017-01-28 22:49:19 -0800

    Fix typo in production static storage setting

--- stornco/settings/prod.py
+++ stornco/settings/prod.py
@@ -18,7 +18,7 @@ STATIC_ROOT = "stornco-prod-static.s3-website-us-west-1.amazonaws.com/"
 STATIC_URL = "stornco-prod-static.s3-website-us-west-1.amazonaws.com/"
 
 # Collects static files to s3.
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 # S3 bucket identity:
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
