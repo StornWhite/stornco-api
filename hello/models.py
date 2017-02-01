7ea2692f9392a806111e1225c99e44f9eebde0a5
Storn White <stornwhite@Storns-MacBook-Pro.local>
2017-02-01 07:49:31 -0800

    Created Hello model

--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Hello(models.Model):
+    """
+    A word that has been echoed through the hello program and the number
+    of times that it has been echoed.
+    """
+    word = models.CharField(
+        max_length=50, db_index=True, unique=True
+    )
+    count = models.IntegerField()
+
+    class Meta:
+        verbose_name = 'Hello'
+        verbose_name_plural = 'Hellos'
+
+    def __str__(self):
+        return "%s: %i" % (self.word, self.count)
