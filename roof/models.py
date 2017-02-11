c305bd88edfe2cbd38270d485d298c117df31f5a
Storn White <stornwhite@Storns-MacBook-Pro.local>
2017-02-10 18:00:30 -0800

    Add validation rules

--- roof/models.py
+++ roof/models.py
@@ -1,4 +1,7 @@
+from decimal import Decimal
+
 from django.db import models
+from django.db.models import validators
 
 
 class Roof(models.Model):
@@ -11,9 +14,83 @@ class Roof(models.Model):
     )
     name = models.CharField(max_length=200)
     shape = models.CharField(max_length=1, choices=SHAPE_CHOICES)
-    base = models.DecimalField(max_digits=5, decimal_places=2)
-    height = models.DecimalField(max_digits=4, decimal_places=2)
-    pitch = models.DecimalField(max_digits=3, decimal_places=1)
-    aspect = models.DecimalField(max_digits=3, decimal_places=1)
-    longitude = models.DecimalField(max_digits=10, decimal_places=7)
-    latitude = models.DecimalField(max_digits=9, decimal_places=7)
+    # Width of planar surface.
+    base = models.DecimalField(
+        max_digits=5, decimal_places=2,
+        validators=[
+            validators.MinValueValidator(
+                limit_value=Decimal('0'),
+                message="Base measurement cannot be less than zero meters."
+            ),
+            validators.MaxValueValidator(
+                limit_value=Decimal('999.99'),
+                message="Base measurement cannot exceed 999.99 meters."
+            )
+        ]
+    )
+    # Height of the planar surface, perpendicular to the base.
+    height = models.DecimalField(
+        max_digits=4, decimal_places=2,
+        validators=[
+            validators.MinValueValidator(
+                limit_value=Decimal('0'),
+                message="Height cannot be less than zero meters."
+            ),
+            validators.MaxValueValidator(
+                limit_value=Decimal('99.99'),
+                message="Height measurement cannot exceed 99.99 meters."
+            )
+        ]
+    )
+    pitch = models.DecimalField(
+        max_digits=3, decimal_places=1,
+        validators=[
+            validators.MinValueValidator(
+                limit_value=Decimal('0'),
+                message="Pitch cannot be less than zero degrees."
+            ),
+            validators.MaxValueValidator(
+                limit_value=Decimal('75'),
+                message="Pitch cannot exceed 75 degrees."
+            )
+        ]
+    )
+    aspect = models.DecimalField(
+        max_digits=3, decimal_places=1,
+        validators=[
+            validators.MinValueValidator(
+                limit_value=Decimal('0'),
+                message="Aspect cannot be less than zero degrees."
+            ),
+            validators.MaxValueValidator(
+                limit_value=Decimal('360'),
+                message="Aspect cannot exceed 360 degrees."
+            )
+        ]
+    )
+    longitude = models.DecimalField(
+        max_digits=10, decimal_places=7,
+        validators=[
+            validators.MinValueValidator(
+                limit_value=Decimal('-180'),
+                message='Longitude cannot be less than -180 degrees.'
+            ),
+            validators.MaxValueValidator(
+                limit_value=Decimal('180'),
+                message='Longitude cannot exceed 180 degrees.'
+            )
+        ]
+    )
+    latitude = models.DecimalField(
+        max_digits=9, decimal_places=7,
+        validators=[
+            validators.MinValueValidator(
+                limit_value=Decimal('-90'),
+                message="Latitude may not be less than -90 degrees."
+            ),
+            validators.MaxValueValidator(
+                limit_value=Decimal('90'),
+                message="Latitude may not exceed 90 degrees."
+            )
+        ]
+    )
