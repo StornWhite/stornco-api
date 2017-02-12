602938987d85ba87cb7f807650af24144476187c
Storn White <stornwhite@Storns-MacBook-Pro.local>
2017-02-12 13:32:25 -0800

    Order roofs by ID

--- roof/views.py
+++ roof/views.py
@@ -9,7 +9,7 @@ class RoofModelViewSet(ModelViewSet):
     """
     API endpoint for creating, viewing, editing, and deleting Roof objects.
     """
-    queryset = Roof.objects.all()
+    queryset = Roof.objects.all().order_by('id')
     serializer_class = RoofSerializer
     # Todo: change to IsAuthenticated
     permission_classes = [AllowAny, ]
